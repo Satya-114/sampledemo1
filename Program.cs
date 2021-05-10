8f712b41e10b9b884da5cd9c350925e5c0644f4f
avinashmaddila00 <avinashmaddila00@gmail.com>
2021-05-10 15:25:35 +0530

    hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sampledemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
